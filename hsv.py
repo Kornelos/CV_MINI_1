a7f0c3e219bdfe4e0b96a9e9944e09356d4f059e
Kornelos <kornel1902@gmail.com>
2019-12-22 16:13:09 +0100

    Plant extraction part

--- hsv.py
+++ hsv.py
@@ -1,33 +1,6 @@
-import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 from colorfilters import HSVFilter
 img = cv2.imread("./multi_plant/rgb_00_00_006_00.png")
 window = HSVFilter(img)
 window.show()
 print(f"Image filtered in HSV between {window.lowerb} and {window.upperb}.")
-# while(True):
-#
-#     # grab the frame
-#     #ret, frame = cap.read()
-#
-#     # get trackbar positions
-#     ilowH = cv2.getTrackbarPos('lowH', 'image')
-#     ihighH = cv2.getTrackbarPos('highH', 'image')
-#     ilowS = cv2.getTrackbarPos('lowS', 'image')
-#     ihighS = cv2.getTrackbarPos('highS', 'image')
-#     ilowV = cv2.getTrackbarPos('lowV', 'image')
-#     ihighV = cv2.getTrackbarPos('highV', 'image')
-#
-#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-#     lower_hsv = np.array([ilowH, ilowS, ilowV])
-#     higher_hsv = np.array([ihighH, ihighS, ihighV])
-#     mask = cv2.inRange(hsv, lower_hsv, higher_hsv)qq
-#
-#     frame = cv2.bitwise_and(frame, frame, mask=mask)
-#
-#     # show thresholded image
-#     cv2.imshow('image', frame)
-#     k = cv2.waitKey(1000) & 0xFF # large wait time to remove freezing
-#     if k == 113 or k == 27:
-#         break
--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-#%matplotlib inline
-from scipy import ndimage
 
 
+# i dont remember writing this, maybe works
 def remove_isolated_pixels(image):
     connectivity = 8
 
@@ -17,37 +16,54 @@ def remove_isolated_pixels(image):
     new_image = image.copy()
 
     for label in range(num_stats):
-        if stats[label,cv2.CC_STAT_AREA] == 1:
+        if stats[label, cv2.CC_STAT_AREA] == 1:
             new_image[labels == label] = 0
 
     return new_image
 
+
+
+
 img = cv2.imread("./multi_plant/rgb_00_00_006_00.png")
-# plt.imshow(img)
-# plt.show()
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 # # Green color
 # Image filtered in HSV between [25 39 43] and [ 62 255 122].
-low_green = np.array([25, 39, 43])
-high_green = np.array([62, 255, 122])
+# 2nd filter Image filtered in HSV between [28 39 56] and [ 62 255 137].
+# Image filtered in HSV between [25 38 42] and [ 72 255 255].
+low_green = np.array([25, 38, 42])
+high_green = np.array([72, 255, 255])
 
 green_mask = cv2.inRange(hsv, low_green, high_green)
 green = cv2.bitwise_and(img, img, mask=green_mask)
-# plt.imshow(green)
-# # plt.show()
-binary = cv2.cvtColor(green, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(binary, (5, 5), 0)
-ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-plt.imshow(th3, cmap='gray', vmin=0, vmax=255)
+
+
+# smooth img
+blur = cv2.GaussianBlur(green_mask, (5, 5), 0)
+# ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+kernel = np.ones((5,5), np.uint8)
+# erosion - deletes noise dilitation - THICC
+img_erosion = cv2.erode(green_mask, kernel, iterations=1)
+img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
+
+plt.imshow(img_erosion)
 plt.show()
-#
-#
-#
-# # plt.subplot(1, 2, 1)
-# # plt.imshow(mask, cmap="gray")
-# # plt.subplot(1, 2, 2)
-# grey = cv2.cvtColor(green, cv2.COLOR_BGR2GRAY)
-# plt.imshow(grey, cmap = plt.cm.gray)
-# plt.show()
+plt.imshow(img_dilation)
+plt.show()
+
+# extract contours
+contours, hierarchy = cv2.findContours(img_dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img, contours, 0, (0, 0, 255), 3)
+
+cont_max = max(contours, key=cv2.contourArea)
+cv2.drawContours(img, cont_max, -1, (0, 255, 0), 3)
+plt.imshow(img)
+plt.show()
+print(img.shape)
+empty = np.zeros((480,480))
+# draw filled plant
+cv2.fillPoly(empty, pts =[cont_max], color=(255,255,255))
+plt.imshow(empty)
+plt.show()
